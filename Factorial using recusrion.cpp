5ca36154c3e41b6831518c134525b728c5515f4e
Naman <namanjn619@gmail.com>
2021-05-28 19:12:03 +0530

    Initial commit

---
+++ Factorial using recusrion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+	if(n==0){
+		return 1;
+	}
+	
+	int smalloutput = factorial(n-1);
+	return n*smalloutput;
+}
+
+int main(){
+	int n;
+	cin>>n;
+	
+	int output = factorial(n);
+	cout<<output<<endl;
+	return 0;
+}
